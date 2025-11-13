f52e706f574f75bfcd311c090c0e7718659cfe05
Muhammad-Munsif <m.munsif4375@gmail.com>
2025-11-13 16:35:16 +0500

    small changes in app.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
-import FormValidation from './FormValidation'
-import { ToastContainer } from 'react-toastify'
+import React from "react";
+import "animate.css";
+import FormValidation from "./FormValidation";
+import { ToastContainer } from "react-toastify";
 
 const App = () => {
   return (
-    <div className='min-h-screen bg-gray-200 py-10'>
-      <FormValidation/>
-      <ToastContainer/>
+    <div className="min-h-screen bg-gray-200 py-10">
+      <FormValidation />
+      <ToastContainer />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/FormValidation.jsx
+++ src/FormValidation.jsx
@@ -86,13 +86,13 @@ const FormValidation = () => {
     const selected = tableData[index];
     setInputData(selected);
     setEditIndex(index);
-    
+
   };
 
   return (
     <>
       {/* FORM */}
-      <div className="bg-white rounded-lg w-full max-w-xl mx-auto p-5 mt-5 shadow-xl">
+      <div className="bg-gray-300 rounded-lg w-full max-w-xl mx-auto p-5 mt-5 shadow-xl">
         <h1 className="text-center font-bold text-2xl mb-5">
           Registration Form
         </h1>
