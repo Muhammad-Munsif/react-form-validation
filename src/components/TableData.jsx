d2df01cfe13feb4105a4f3361cb0fc7c5659ed99
Muhammad-Munsif <m.munsif4375@gmail.com>
2025-11-17 17:36:31 +0500

    small changes and added for small code to registration component file

--- src/components/TableData.jsx
+++ src/components/TableData.jsx
@@ -24,21 +24,21 @@ const DataTable = ({ data, onEdit, onDelete }) => {
       {/* Desktop Table with Custom Scrollbar */}
       <div className="hidden sm:block max-h-[200px] overflow-y-auto border border-gray-200 rounded-lg shadow scrollbar-thin scrollbar-thumb-gray-300 scrollbar-track-gray-100 scrollbar-thumb-rounded">
         {/* Add Search Bar in DataTable */}
-        
-          <div className="relative">
-            <Search
-              className="absolute left-3 top-1/2 transform -translate-y-1/2 text-black font-semibold"
-              size={20}
-            />
-            <input
-              type="text"
-              placeholder="Search in records..."
-              value={searchTerm}
-              onChange={(e) => setSearchTerm(e.target.value)}
-              className="w-full p-3 pl-10 border border-gray-300 rounded-lg focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 outline-none bg-white"
-            />
-          </div>
-    
+
+        <div className="relative">
+          <Search
+            className="absolute left-3 top-1/2 transform -translate-y-1/2 text-black font-semibold"
+            size={20}
+          />
+          <input
+            type="text"
+            placeholder="Search in records..."
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            className="w-full p-3 pl-10 border border-gray-300 rounded-lg focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 outline-none bg-white"
+          />
+        </div>
+
         <table className="min-w-full divide-y divide-gray-200">
           <thead className="bg-gray-100 sticky top-0 z-10">
             <tr>
