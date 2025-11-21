30c50b090ed97a840f9fa1b91f275292e00d28a0
Muhammad-Munsif <m.munsif4375@gmail.com>
2025-11-21 16:29:03 +0500

    small changes and create a db.json file

--- src/components/TableJson.jsx
+++ src/components/TableJson.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
 
 const TableJson = () => {
+    const [columns, setColumns] = useState({
+        id : id,
+        name : '',
+        email : '',
+        password : ''
+    })
+    const [records, setRecords] = useState()
+    axios.get('http://localhost:5000/users')
+    .then(res => setColumns(res.data))
+    .catch(err => console.error("this is a problem")
+    )
   return (
     <div>
-      
+      <table>
+        <thead>
+            {
+                columns.map()
+            }
+        </thead>
+      </table>
     </div>
   )
 }
