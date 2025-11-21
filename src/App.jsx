765540bfafbcdce5752eccefd96a71031df5e9c7
Muhammad-Munsif <m.munsif4375@gmail.com>
2025-11-21 17:42:58 +0500

    small changes and create a db.json file

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import "animate.css";
 import FormValidation from "./components/FormValidation";
 import { ToastContainer } from "react-toastify";
+import TableJson from "./components/TableJson";
 
 const App = () => {
   return (
     <div className="min-h-screen bg-gray-200 py-10 h-screen no-scrollbar overflow-y-scroll">
+      <TableJson/>
       <FormValidation />
       <ToastContainer />
     </div>
