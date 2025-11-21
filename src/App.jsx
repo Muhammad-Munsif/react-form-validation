45828c30aaf04bf23a727bc0d6bc5aa47178588a
Muhammad-Munsif <m.munsif4375@gmail.com>
2025-11-21 17:59:40 +0500

    small changes and create a TableJson  file

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,7 @@ import TableJson from "./components/TableJson";
 const App = () => {
   return (
     <div className="min-h-screen bg-gray-200 py-10 h-screen no-scrollbar overflow-y-scroll">
-      
-      <TableJson/>
+      <TableJson />
       <FormValidation />
       <ToastContainer />
     </div>
