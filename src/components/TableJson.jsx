20d9713162f7fb54d8e67a65de33e54ba5f0762e
Muhammad-Munsif <m.munsif4375@gmail.com>
2025-11-24 12:29:09 +0500

    changes in TableJson component

--- src/components/TableJson.jsx
+++ src/components/TableJson.jsx
@@ -16,24 +16,32 @@ const TableJson = () => {
   }, []);
 
   return (
-    <div>
-      <table border="1" cellPadding="8">
-        <thead>
+    <div className="flex items-center justify-center">
+      <table striped="columns" className="border border-gray-500">
+        <thead className="bg-amber-50 p-4">
           <tr>
             {columns.map((c, i) => (
               <th key={i}>{c}</th>
             ))}
+            <td>action</td>
           </tr>
         </thead>
 
-        <tbody>
+        <tbody className="p-4">
           {records.map((item, index) => (
             <tr key={index}>
               <td>{item.id}</td>
               <td>{item.name}</td>
               <td>{item.email}</td>
               <td>{item.password}</td>
+              <td>
+                <div>
+                  <button className="bg-red-6 px-4 py-2 rounded-lg text-center font-semibold">edit</button>
+                  <button className="bg-red-6 px-4 py-2 rounded-lg text-center font-semibold">delete</button>
+                </div>
+              </td>
             </tr>
+
           ))}
         </tbody>
       </table>
